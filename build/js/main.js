fe214a4ea2f281db4bc9c4bbc16f3a94bd10df01
MoWael11 <midonit06@gmail.com>
2023-05-20 12:05:25 +0200

    initial commit

---
+++ build/js/main.js
@@ -0,0 +1,82 @@
+"use strict";
+const buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+const buttons = document.querySelectorAll('.game');
+const playButton = document.getElementById('play');
+const score = document.getElementById('score');
+const aside = document.querySelector('aside');
+const text = document.getElementById('level-title');
+const body = document.body;
+const rulesButton = document.getElementById('rules');
+const rules = document.getElementById('rules-content');
+const rulesButtonClose = document.getElementById('rules-close');
+const nextSequence = () => {
+    userClickedPattern = [];
+    level++;
+    text.textContent = `Level ${level}`;
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+};
+const playSound = (name) => {
+    var audio = new Audio();
+    audio.src = `../sounds/${name}.mp3`;
+    audio.play();
+};
+const animatePress = (currentColor) => {
+    const button = document.getElementById(currentColor);
+    button.classList.add('pressed');
+    setTimeout(() => {
+        button.classList.remove('pressed');
+    }, 100);
+};
+const checkAnswer = (currentLevel) => {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => { nextSequence(); }, 1000);
+        }
+    }
+    else {
+        playSound('wrong');
+        body.classList.add('game-over');
+        text.textContent = `You lost!, with a score of ${level}`;
+        setTimeout(() => {
+            body.classList.remove('game-over');
+        }, 200);
+        started = false;
+        gamePattern = [];
+        aside.classList.remove('hide');
+        if (+(score.innerHTML) < level) {
+            score.innerHTML = level.toString();
+        }
+        level = 0;
+    }
+};
+playButton.addEventListener('click', () => {
+    started = true;
+    aside.classList.add('hide');
+    text.textContent = `Level ${level}`;
+    nextSequence();
+});
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.id && started) {
+            const userChosenColour = e.target.id;
+            userClickedPattern.push(userChosenColour);
+            playSound(userChosenColour);
+            animatePress(userChosenColour);
+            checkAnswer(userClickedPattern.length - 1);
+        }
+    });
+});
+rulesButton.addEventListener('click', () => {
+    rules.classList.remove('hide');
+});
+rulesButtonClose.addEventListener('click', () => {
+    rules.classList.add('hide');
+});
---
+++ src/main.ts
@@ -0,0 +1,90 @@
+const buttonColours:Array<string> = ["red", "blue", "green", "yellow"]
+
+let gamePattern:Array<string> = []
+let userClickedPattern:Array<string> = []
+
+let level:number = 0
+let started:boolean = false
+
+const buttons = document.querySelectorAll('.game') as NodeListOf<HTMLDivElement>
+const playButton = document.getElementById('play') as HTMLDivElement
+const score = document.getElementById('score') as HTMLSpanElement
+const aside = document.querySelector('aside') as HTMLDivElement
+const text = document.getElementById('level-title') as HTMLHeadingElement
+const body = document.body as HTMLBodyElement
+const rulesButton = document.getElementById('rules') as HTMLDivElement 
+const rules = document.getElementById('rules-content') as HTMLDivElement
+const rulesButtonClose = document.getElementById('rules-close') as HTMLDivElement
+
+const nextSequence = () => {
+  userClickedPattern = []
+  level++
+  text.textContent = `Level ${level}`
+  const randomNumber = Math.floor(Math.random() * 4)
+  const randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour)
+  playSound(randomChosenColour)
+  animatePress(randomChosenColour)
+}
+
+const playSound = (name: string) => {
+  var audio = new Audio()
+  audio.src = `../sounds/${name}.mp3`
+  audio.play()
+}
+
+const animatePress = (currentColor: string) => {
+  const button = document.getElementById(currentColor) as HTMLDivElement
+  button.classList.add('pressed')
+  setTimeout(() => { 
+    button.classList.remove('pressed')
+  }, 100)
+}
+
+const checkAnswer = (currentLevel: number) => {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout( () => {nextSequence()}, 1000)}
+    } else {
+    playSound('wrong')
+    body.classList.add('game-over')
+    text.textContent = `You lost!, with a score of ${level}`
+    setTimeout(() => {
+      body.classList.remove('game-over')
+    }, 200);
+    started = false
+    gamePattern = []
+    aside.classList.remove('hide')
+    if (+(score.innerHTML) < level) {
+      score.innerHTML = level.toString()
+    }
+    level = 0
+  }
+} 
+
+playButton.addEventListener('click', () => {
+  started = true
+  aside.classList.add('hide')
+  text.textContent = `Level ${level}`
+  nextSequence();
+})
+
+buttons.forEach(button => {
+  button.addEventListener('click', (e: MouseEvent) => {
+    if ((e.target as HTMLDivElement).id && started) {
+      const userChosenColour = (e.target as HTMLDivElement).id
+      userClickedPattern.push(userChosenColour)
+      playSound(userChosenColour)
+      animatePress(userChosenColour)
+      checkAnswer(userClickedPattern.length - 1)
+    }
+  })
+})
+
+rulesButton.addEventListener('click', () => {
+  rules.classList.remove('hide')
+})
+
+rulesButtonClose.addEventListener('click', () => {
+  rules.classList.add('hide')
+})
